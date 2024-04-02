2a2098093af3aa4e1aac1a3559a9ba4b6ed60981
Retread2000 <153476084+Retread2000@users.noreply.github.com>
2024-04-02 15:40:47 -0400

    Update Quest 3.cpp
    Lots of bug fixes.
    Added some functions.
    Code runs now.
    Issue with processing user input - does not iterate through the first character.

--- Quest 3.cpp
+++ Quest 3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <limits>
 
 using namespace std;
 
@@ -9,33 +10,47 @@ class CaesarCipher { //base class
 
     protected:
         string encode(const string& message) { //encode message function
-            string newMsg;
+            string newMsg = "";
             for (int i = 0; i < message.size(); i++) { //iterate through message string
                 if (int(message[i]) > 65 && int(message[i]) < 84) { //use ascii and shift value to create encoded string
-                    newMsg[i] = char(message[i] + shiftValue);
+                    newMsg.push_back(char(message[i] + shiftValue));
                 }
                 else if (int(message[i]) > 83 && int(message[i]) < 91) { //condition for the letters at the end of alphabet
-                    newMsg[i] = char(int(message[i]) - 19);
+                    newMsg.push_back(char(int(message[i]) - 19));
                 }
             }
             return newMsg;
         }
 
     public:
-        //constructor
-        CaesarCipher(int value) : shiftValue(value) {}
+        CaesarCipher(int value) : shiftValue(value) {} //constructor
         void setShiftValue(int value) { //setter
             shiftValue = value;
         }
+        string getEncode(const string& message) {
+            return encode(message);
+        }
         string decode(const string& message) { //decode message function
             string eMsg = encode(message);
-            string dMsg;
-            for (int i = 0; i < eMsg.size(); i++) {
-                if (int(eMsg[i]) > 71 && int(eMsg[i]) < 91) { //use ascii and shift value to create decoded string
-                    dMsg[i] = char(eMsg[i] - shiftValue);
+            string dMsg = "";
+            if (!eMsg.empty()) {
+                for (int i = 0; i < eMsg.size(); i++) {
+                    if (int(eMsg[i]) > 71 && int(eMsg[i]) < 91) { //use ascii and shift value to create decoded string
+                        dMsg += (char(eMsg[i] - shiftValue));
+                    }
+                    else if (int(eMsg[i]) > 64 && int(eMsg[i]) < 72) { //condition for the letters at the beginning of alphabet
+                        dMsg += (char(int(eMsg[i]) + 19));
+                    }
                 }
-                else if (int(eMsg[i]) > 64 && int(eMsg[i]) < 72) { //condition for the letters at the beginning of alphabet
-                    dMsg[i] = char(int(eMsg[i]) + 19);
+            }
+            else if (eMsg.empty()) {
+                for (int i = 0; i < message.size(); i++) {
+                    if (int(message[i]) > 71 && int(message[i]) < 91) { //use ascii and shift value to create decoded string
+                        dMsg[i] = char(message[i] - shiftValue);
+                    }
+                    else if (int(message[i]) > 64 && int(message[i]) < 72) { //condition for the letters at the beginning of alphabet
+                        dMsg[i] = char(int(message[i]) + 19);
+                    }
                 }
             }
             return dMsg;
@@ -44,34 +59,94 @@ class CaesarCipher { //base class
 
 class Rot13Cipher : public CaesarCipher { //derived class
     public:
-    Rot13Cipher() : CaesarCipher(13) {}
+    //Rot13Cipher() : CaesarCipher(13) {}
 };
 
 bool encodePrompt() {
-    string uInput;
-    bool p;
+    string uInput = "";
+    bool p = false;
     cout << "Do you want to encode a new message? (y/n or yes/no): ";
     cin >> uInput;
-    if (uInput == "y" || uInput == "yes" || uInput == "Yes") {
+    bool condition = true;
+    while (condition == true) {
+        if (uInput == "y" || uInput == "Y" || uInput == "yes" || uInput == "Yes") {
+            p = true;
+            condition = false;
+        }
+        else if (uInput == "n" || uInput == "N" || uInput == "no" || uInput == "No") {
+            p = false;
+            condition = false;
+        }
+        else {
+            cout << "Invalid input. Please enter y, n, yes, or no: ";
+            condition = true;
+            cin >> uInput;
+        }
+    }
+    return p;
+}
+
+bool decodePrompt(bool e) {
+    string uInput;
+    bool p = false;
+    if (!e) {
+        cout << "Do you want to decode a message? (y/n or yes/no): ";
+    }
+    else if (e) {
+        cout << "Do you want to decode the recently captured message? (y/n or yes/no): ";
+    }
+    cin >> uInput;
+    if (uInput == "y" || uInput == "Y" || uInput == "yes" || uInput == "Yes") {
         p = true;
     }
-    else if (uInput == "n" || uInput == "no" || uInput == "No") {
+    else if (uInput == "n" || uInput == "N" || uInput == "no" || uInput == "No") {
         p = false;
     }
     return p;
 }
 
+void runEncode() {
+    string message;
+    int value;
+    cout << "Enter the message to be encoded: ";
+    cin >> message;
+    cout << "Enter a shift value (1-13) to encode the message accordingly: ";
+    cin >> value;
+    CaesarCipher cipher(value);
+    cout << "Encoded message using Caesar Cipher (shift value " << value << "): " << cipher.getEncode(message) << endl;
+}
+
+void runDecode(bool e) {
+    int value;
+    string message;
+    if (!e) {
+        cout << "Enter the captured message: ";
+        cin >> message;
+    }
+    cout << "Enter your guess for the shift value (1-13) to decode the message: ";
+    cin >> value;
+    cout << "Decoding message using Caesar Cipher:\n";
+    CaesarCipher cipher(value);
+    cout << "Encoded message: " << message << endl;
+    cout << "Decoded message: " << cipher.decode(message) << endl;
+}
+
+
 int main() { //work in progress
     cout << "~~~~~~~~~~ Welcome to Guild 20's Cipher Program! ~~~~~~~~~~\n\n";
-    bool ePrompt = encodePrompt();
-    if (ePrompt == true) {
-
+    bool ePrompt = true;
+    bool dPrompt = true;
+    while (ePrompt == true || dPrompt == true) {
+        ePrompt = encodePrompt();
+        if (ePrompt == true) {
+            runEncode();
+        }
+        dPrompt = decodePrompt(ePrompt);
+        if (dPrompt == true) {
+            runDecode(ePrompt);
+        }
     }
-    else if (ePrompt == false) {
-
-    }
-
-
-
+    cout << "The end.";
+    return 0;
 }
 
